7f0664fa6efb71e473ec66ad51b91f9a11ee05e1
Pedro Conde <pconde007@gmail.com>
2023-08-14 20:27:20 +0100

    Final sección 11

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { PrimeNgModule } from './../prime-ng/prime-ng.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+
+
+
+@NgModule({
+  declarations: [
+
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    PrimeNgModule
+  ],
+  exports: [
+    MenuComponent
+  ]
+})
+export class SharedModule { }
